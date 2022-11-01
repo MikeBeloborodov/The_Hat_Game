dbf9fbf228a0689b87c2835503c8a03e3b591c40
Mikhail Beloborodov <kiritohenajani@gmail.com>
2022-11-01 09:16:01 +0400

    cleaned client app

--- client/src/main.ts
+++ client/src/main.ts
@@ -2,5 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from 'axios'
 
-createApp(App).use(store).use(router).mount('#app')
+const app = createApp(App)
+
+axios.defaults.baseURL = process.env.VUE_APP_DJANGO_HOST
+
+app.use(store).use(router, axios).mount('#app')
